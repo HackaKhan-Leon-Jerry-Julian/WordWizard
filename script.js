f1e7bd88df95d26d7c39b538604c190cb279692f
leonwang <83103060+lwang393@users.noreply.github.com>
2023-06-10 15:56:53 -0700

    logo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const addQuestionCard = document.getElementById("add-question-card");
+const defocusBackground = document.getElementById("defocus-background");
+const cardQuestionContainer = document.getElementById("add-question-card");
+const ioContainer = document.getElementById("io-container");
+const flashcardTitle = document.getElementById("flashcard-title");
 const addQuestion = document.getElementById("add-flashcard");
 
 const saveButton = document.getElementById("save-btn");
@@ -8,9 +11,15 @@ const question = document.getElementById("question");
 // const errorMessage = document.getElementById("error");
 
 // const closeBtn = document.getElementById("close-btn");
-// const exportBtn = document.getElementById("export");
+
+const ioButton = document.getElementById("io");
+const confirm = document.getElementById("confirm");
+const code = document.getElementById("code");
 
 var cards = [];
+var current = -1;
+var binded = -1;
+var consumed = false;
 
 // //Add question when user clicks 'Add Flashcard' button
 // addQuestion.addEventListener("click", () => {
@@ -19,98 +28,155 @@ var cards = [];
 // //Hide Create flashcard Card
 // //Submit Question
 
+document.onmouseup = () => {
+  consumed = false;
+};
+
 addQuestion.addEventListener("click", () => {
-  addQuestionCard.style.display = "flex";
+  if(consumed) return;
+  consumed = true;
+  defocusBackground.style.display = "block";
+  cardQuestionContainer.style.display = "flex";
+  flashcardTitle.innerText = "Add Flashcard";
 });
 
 saveButton.addEventListener("click", () => {
-  cards[cards.length] = [question.value, answer.value];
+  if(consumed) return;
+  consumed = true;
+  if(binded >= 0){
+    cards[binded] = [question.value, answer.value, false];
+    binded = -1;
+  }else{
+    cards[cards.length] = [question.value, answer.value, false];
+  }
   rebuild();
-  addQuestionCard.style.display = "none";
+  defocusBackground.style.display = "none";
+  cardQuestionContainer.style.display = "none";
   question.value = "";
   answer.value = "";
 });
 
 closeButton.addEventListener("click", () => {
-  addQuestionCard.style.display = "none";
+  if(consumed) return;
+  consumed = true;
+  defocusBackground.style.display = "none";
+  cardQuestionContainer.style.display = "none";
+  current = -1;
+  binded = -1;
 });
 
+ioButton.addEventListener("click", () => {
+  if(consumed) return;
+  consumed = true;
 
+  var out = "";
+  for(var i = 0;i < cards.length;i++){
+    out += cards[i][0] + '\n';
+    out += cards[i][1] + '\n';
+  }
+  code.value = out;
 
-// exportBtn.addEventListener(
-//   "click",
-//   (exportCards = () => {
-//     var listCard = document.getElementsByClassName("card-list-container");
-//     var div = document.createElement("div");
-//     div.innerHTML += `
-//     <p class="goofy-div">sus</p>`;
-//     listCard[0].appendChild(div);
-//   })
-// );
-// //Card Generate
+  defocusBackground.style.display = "block";
+  ioContainer.style.display = "flex";
+});
+
+confirm.addEventListener("click", () => {
+  if(consumed) return;
+  consumed = true;
+
+  cards = [];
+  for(var i = 0;i < code.value.length;){
+    var a = "";
+    var b = "";
+    for(var j = 0;j < 2;j++){
+      var current = "";
+      while(i < code.value.length && code.value[i] !== '\n'){
+        current += code.value[i];
+        i++;
+      }
+      i++;
+      if(j === 0){
+        a = current;
+      }else{
+        b = current;
+      }
+    }
+    cards[cards.length] = [a, b, false]
+  }
+  rebuild();
+
+  defocusBackground.style.display = "none";
+  ioContainer.style.display = "none";
+});
+
 
 function rebuild(){
   var list = document.getElementById("card-list-container");
   list.innerHTML = '';
 
   for(var i = 0;i < cards.length;i++){
-    var div = document.createElement("div");
-    div.innerHTML += '<p class="question-div">${questions[i]}</p>';
+    var card = document.createElement('button');
+    card.classList.add("card");
 
-    list.appendChild(div);
+    card.innerHTML += '<p class="card-text">' + (cards[i][2] ? cards[i][1] : cards[i][0]) + '</p>'
+
+    var edit = document.createElement('button');
+    edit.classList.add("edit");
+    edit.innerHTML = '<i class="fa-solid fa-pen-to-square" style="font-size: 3em;"></i>';
+    card.appendChild(edit);
+
+    var trash = document.createElement('button');
+    trash.classList.add("delete");
+    trash.innerHTML = '<i class="fa-solid fa-trash-can" style="font-size: 3em;"></i>';
+    card.appendChild(trash);
+
+    if(current === i){
+      edit.style.display = "flex";
+      trash.style.display = "flex";
+    }
+
+    const real = i, realEdit = edit, realTrash = trash;
+
+    edit.addEventListener("click", () => {
+      if(consumed) return;
+      consumed = true;
+      question.value = cards[real][0];
+      answer.value = cards[real][1];
+      defocusBackground.style.display = "block";
+      cardQuestionContainer.style.display = "flex";
+      binded = real;
+      current = -1;
+      flashcardTitle.innerText = "Edit Flashcard";
+    });
+
+    trash.addEventListener("click", () => {
+      if(consumed) return;
+      consumed = true;
+      cards.splice(real, 1);
+      sus();
+    });
+
+    card.addEventListener("mouseover", () => {
+      realEdit.style.display = "flex";
+      realTrash.style.display = "flex";
+      current = real;
+    });
+    card.addEventListener("mouseout", () => {
+      realEdit.style.display = "none";
+      realTrash.style.display = "none";
+      current = -1;
+    });
+    card.addEventListener("click", () => {
+      if(consumed) return;
+      consumed = true;
+      cards[real][2] = !cards[real][2];
+      sus();
+    });
+
+    list.appendChild(card);
   }
 }
 
-//   for(var i = 0;i < questoons.length;i++){
-//     var div = document.createElement("div");
-//     div.classList.add("card");
-//     div.innerHTML += `<p class="question-div">${questions[i]}</p>`;
-
-//     listCard[0].appendChild(div);
-//     hideQuestion();
-//   }
-// }
-
-  // var div = document.createElement("div");
-  // div.classList.add("card");
-  // //Question
-  // div.innerHTML += `
-  // <p class="question-div">${question.value}</p>`;
-  // //Answer
-  // var displayAnswer = document.createElement("p");
-  // displayAnswer.classList.add("answer-div", "hide");
-  // displayAnswer.innerText = answer.value;
-  // //Link to show/hide answer
-  // var link = document.createElement("a");
-  // link.setAttribute("href", "#");
-  // link.setAttribute("class", "show-hide-btn");
-  // link.innerHTML = "Show/Hide";
-  // link.addEventListener("click", () => {
-  //   displayAnswer.classList.toggle("hide");
-  // });
-  // div.appendChild(link);
-  // div.appendChild(displayAnswer);
-  // //Edit button
-  // let buttonsCon = document.createElement("div");
-  // buttonsCon.classList.add("buttons-con");
-  // var editButton = document.createElement("button");
-  // editButton.setAttribute("class", "edit");
-  // editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
-  // editButton.addEventListener("click", () => {
-  //   editBool = true;
-  //   modifyElement(editButton, true);
-  //   addQuestionCard.classList.remove("hide");
-  // });
-  // buttonsCon.appendChild(editButton);
-  // disableButtons(false);
-  // //Delete Button
-  // var deleteButton = document.createElement("button");
-  // deleteButton.setAttribute("class", "delete");
-  // deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-  // deleteButton.addEventListener("click", () => {
-  //   modifyElement(deleteButton);
-  // });
-  // buttonsCon.appendChild(deleteButton);
-  // div.appendChild(buttonsCon);
-  // listCard[0].appendChild(div);
-  // hideQuestion();
+function sus(){
+  rebuild();
+}
